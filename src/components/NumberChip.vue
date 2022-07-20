00040b89814152918edabff9085713e90035fa21
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-07-20 18:50:29 +0500

    positioned numbers on the image

---
+++ src/components/NumberChip.vue
@@ -0,0 +1,36 @@
+<template>
+  <span class="number" :style="{ top: this.y + 'px', left: this.x + 'px' }">{{
+    this.name
+  }}</span>
+</template>
+
+<script>
+export default {
+  name: "NumberChip",
+  props: {
+    name: {
+      type: String,
+      default: "*",
+    },
+    x: {
+      type: Number,
+      required: true,
+    },
+    y: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.number {
+  position: absolute;
+  padding: 4px 8px;
+  font-weight: bold;
+  background-color: white;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/OfficeMap.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="image-container">
+    <img alt="Vue logo" src="./../assets/image.png" />
+    <div class="number-layer">
+      <NumberChip
+        v-for="coord in coords"
+        :key="coord.id"
+        :x="coord.x"
+        :y="coord.y"
+        :name="coord.name"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import NumberChip from "./NumberChip.vue";
+export default {
+  name: "OfficeMap",
+  data() {
+    return {
+      coords: [
+        { x: 490, y: 264, id: 1, name: "1" },
+        { x: 734, y: 205, id: 2, name: "2" },
+        { x: 690, y: 365, id: 3, name: "3" },
+      ],
+    };
+  },
+  components: { NumberChip },
+};
+</script>
+
+<style scoped>
+:root {
+  --bg-color: rgba(201, 201, 201, 1);
+}
+
+.image-container {
+  display: flex;
+  position: relative;
+  flex: 1;
+  justify-content: center;
+  margin: 16px;
+  border-radius: 16px;
+  border: 1px solid var(--bg-color);
+
+  -webkit-box-shadow: -2px 6px 26px -10px rgba(201, 201, 201, 1);
+  -moz-box-shadow: -2px 6px 26px -10px rgba(201, 201, 201, 1);
+  box-shadow: -2px 6px 26px -10px rgba(201, 201, 201, 1);
+}
+.number-layer {
+  position: absolute;
+  width: 927px;
+  height: 657px;
+}
+.number {
+  position: absolute;
+  padding: 4px 8px;
+  background-color: white;
+  border-radius: 5px;
+}
+</style>
